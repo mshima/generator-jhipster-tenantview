5b592a0ddd6c0274b3fe7377a4ae444527aaecfb
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-10-30 14:02:21 -0200

    Update faker patch

--- generators/entity-server/patcher/files.js
+++ generators/entity-server/patcher/files.js
@@ -1,13 +1,47 @@
 const randexp = require('randexp');
 const debug = require('debug')('tenantview:entity:server:files');
+const faker = require('faker');
 
-const faker = require('../../faker');
 const jhipsterEnv = require('../../jhipster-environment');
 
 const jhipsterConstants = jhipsterEnv.constants;
 
 // In order to have consistent results with Faker, the seed is fixed.
 faker.seed(42);
+
+/*
+ * Current faker version is 4.1.0 and was release in 2017
+ * It is outdated
+ * https://github.com/Marak/faker.js/blob/10bfb9f467b0ac2b8912ffc15690b50ef3244f09/lib/date.js#L73-L96
+ * Needed for reproducible builds
+ */
+/* eslint-disable-next-line */
+const recentDate = function (days, refDate) {
+    /* eslint-disable */
+    var date = new Date();
+    if (typeof refDate !== "undefined") {
+        date = new Date(Date.parse(refDate));
+    }
+
+    var range = {
+            min: 1000,
+            max: (days || 1) * 24 * 3600 * 1000
+    };
+
+    var future = date.getTime();
+    future -= faker.random.number(range); // some time from now to N days ago, in milliseconds
+    date.setTime(future);
+
+    return date;
+};
+
+const recentForLiquibase = function (days, changelogDate) {
+    let formatedDate;
+    if (changelogDate !== undefined) {
+        formatedDate = changelogDate.substring(0, 4) + '-' + changelogDate.substring(4, 6) + '-' + changelogDate.substring(6, 8);
+    }
+    return recentDate(1, formatedDate);
+}
 
 module.exports = {
     files: {
@@ -22,6 +56,8 @@ module.exports = {
                             interpolate: jhipsterConstants.INTERPOLATE_REGEX,
                             context: {
                                 faker,
+                                recentDate,
+                                recentForLiquibase,
                                 debug,
                                 randexp
                             }
