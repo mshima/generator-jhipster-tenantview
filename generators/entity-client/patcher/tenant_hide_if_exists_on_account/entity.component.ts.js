966759a898257c06430f65e5fc1e0c331a545693
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-11-11 20:33:09 -0200

    Readd currentAccount to tenantAware on 6.5.0

---
+++ generators/entity-client/patcher/tenant_hide_if_exists_on_account/entity.component.ts.js
@@ -0,0 +1,40 @@
+const file = context => `${context.CLIENT_MAIN_SRC_DIR}app/entities/${context.entityFolderName}/${context.entityFileName}.component.ts`;
+
+const condition = context => context.tenantAware;
+
+const tmpls = [
+    {
+        // Add imports account
+        type: 'rewriteFile',
+        target: 'import { JhiEventManager',
+        tmpl: context => "import { AccountService } from 'app/core/auth/account.service';"
+    },
+    {
+        // Add currentAccount field
+        type: 'replaceContent',
+        regex: true,
+        target: /(implements OnInit, OnDestroy {\n(\s*))/,
+        tmpl: '$1$2currentAccount: any;\n'
+    },
+    {
+        type: 'replaceContent',
+        target: /(constructor\(\n(\s*))/,
+        tmpl: context => '$1$2private accountService: AccountService,\n'
+    },
+    {
+        type: 'replaceContent',
+        target: /(\n(\s*)ngOnInit\(\) {\n(\s*)this.loadAll\(\);)/,
+        tmpl: context => `$1
+$3this.accountService.identity().subscribe((account) => {
+$2$3this.currentAccount = account;
+$3});
+$3`
+    }
+];
+
+module.exports = {
+    version: '^6.5.0',
+    file,
+    condition,
+    tmpls
+};
