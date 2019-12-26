f0678fc0c49cd54c188742e28dddc0d512b5fb2c
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-12-26 19:51:10 -0200

    v6.6.0 support: add v6.6.0 navbar.component.ts.js

---
+++ generators/client/patcher/angular/tenant_add_to_account/navbar.component.ts.js
@@ -0,0 +1,18 @@
+const file = context => `${context.CLIENT_MAIN_SRC_DIR}app/layouts/navbar/navbar.component.ts`;
+
+const tmpls = [
+    {
+        type: 'rewriteFile',
+        target: context => 'getImageUrl(): string {',
+        tmpl: context => `has${context.tenantNameUpperFirst}(): boolean {
+    return this.isAuthenticated() && this.accountService.get${context.tenantNameUpperFirst}() ? true : false;
+  }
+`
+    }
+];
+
+module.exports = {
+    version: '>=6.6.0',
+    file,
+    tmpls
+};
