ddeb836769c52a8904203b795a9f135c41429b31
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-12-27 11:38:22 -0200

    v6.6.0 support: add v6.6.0 account.service.spec.ts.js

---
+++ generators/client/patcher/angular/tenant_add_to_account/account.service.spec.ts.js
@@ -0,0 +1,20 @@
+const jhipsterEnv = require('generator-jhipster-customizer');
+
+const jhipsterConstants = jhipsterEnv.constants;
+
+const file = context => `${jhipsterConstants.CLIENT_TEST_SRC_DIR}spec/app/core/user/account.service.spec.ts`;
+
+const tmpls = [
+    {
+        type: 'replaceContent',
+        target: /(\n(\s*)imageUrl: '')(,?)/,
+        tmpl: context => `$1,
+$2${context.tenantNameLowerFirst}: ''$3`
+    }
+];
+
+module.exports = {
+    version: '>=6.6.0',
+    file,
+    tmpls
+};
