80ed2e787b12ef38b54ed53ac2406c3c4d3303b3
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-12-27 11:33:12 -0200

    v6.6.0 support: add v6.6.0 mock-account.service.ts.js

---
+++ generators/client/patcher/angular/tenant_add_to_account/protractor/mock-account.service.ts.js
@@ -0,0 +1,34 @@
+const jhipsterEnv = require('generator-jhipster-customizer');
+
+const jhipsterConstants = jhipsterEnv.constants;
+
+const file = context => `${jhipsterConstants.CLIENT_TEST_SRC_DIR}spec/helpers/mock-account.service.ts`;
+
+const tmpls = [
+    {
+        type: 'replaceContent',
+        target: /((\n\s*)getAuthenticationStateSpy: Spy;)/,
+        tmpl: '$2hasAnyAuthoritySpy: Spy;$1'
+    },
+    {
+        type: 'replaceContent',
+        target: /((\n\s*)this.getAuthenticationStateSpy = )/,
+        tmpl: "$2this.hasAnyAuthoritySpy = this.spy('hasAnyAuthority').andReturn(of(true));$1"
+    },
+    {
+        type: 'replaceContent',
+        target: /(\n(\s*)setIdentityResponse)/,
+        tmpl: `
+$2setHasAnyAuthority(hasAutority: boolean): void {
+$2$2this.hasAnyAuthoritySpy = this.spy('hasAnyAuthority').andReturn(of(hasAutority));
+$2}
+$1`
+    }
+];
+
+module.exports = {
+    version: '>=6.6.0',
+    condition: context => context.testFrameworks.indexOf('protractor') !== -1,
+    file,
+    tmpls
+};
