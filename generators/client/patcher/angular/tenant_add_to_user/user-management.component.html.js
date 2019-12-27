e5221de4577eb0758ba2f5f5b2a8b7877d8cc9d1
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-12-27 20:01:30 -0200

    v6.6.0 support: fix user-management.component.html.js

--- generators/client/patcher/angular/tenant_add_to_user/user-management.component.html.js
+++ generators/client/patcher/angular/tenant_add_to_user/user-management.component.html.js
@@ -7,7 +7,7 @@ const tmpls = [
         target: context => '((.*)<th(.*)>\\s*<span jhiTranslate="userManagement.profiles">)',
         tmpl: context => {
             // eslint-disable-next-line prettier/prettier
-            return `$2<th$3 jhiSortBy="${context.tenantNameLowerFirst}" *ngIf="!currentAccount.${context.tenantNameLowerFirst}"><span jhiTranslate="userManagement.${context.tenantNameLowerFirst}">${context.tenantNameUpperFirst}</span> <fa-icon [icon]="'sort'"></fa-icon></th>
+            return `$2<th$3 jhiSortBy="${context.tenantNameLowerFirst}" *ngIf="!currentAccount || !currentAccount.${context.tenantNameLowerFirst}"><span jhiTranslate="userManagement.${context.tenantNameLowerFirst}">${context.tenantNameUpperFirst}</span> <fa-icon [icon]="'sort'"></fa-icon></th>
 $1`;
         }
     },
@@ -16,7 +16,9 @@ $1`;
         regex: true,
         target: /((\s*)<td>(\s*)<div \*ngFor="let authority of user.authorities">)/,
         tmpl: context =>
-            `$2<td *ngIf="!currentAccount.${context.tenantNameLowerFirst}">{{user.${context.tenantNameLowerFirst}?.name}}</td>$1`
+            `$2<td *ngIf="!currentAccount || !currentAccount.${context.tenantNameLowerFirst}">{{user.${
+                context.tenantNameLowerFirst
+            }?.name}}</td>$1`
     }
 ];
 
