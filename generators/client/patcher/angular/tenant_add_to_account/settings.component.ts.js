ab4873474883b59ee4c5701552c01b79ab34d073
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-12-27 11:30:18 -0200

    v6.6.0 support: add v6.6.0 settings.component.ts.js

---
+++ generators/client/patcher/angular/tenant_add_to_account/settings.component.ts.js
@@ -0,0 +1,32 @@
+const jhipsterEnv = require('generator-jhipster-customizer');
+
+const jhipsterConstants = jhipsterEnv.constants;
+
+const file = context => `${jhipsterConstants.CLIENT_MAIN_SRC_DIR}app/account/settings/settings.component.ts`;
+
+const tmpls = [
+    {
+        type: 'replaceContent',
+        target: /(\n(\s*)email: \[.*\]\])(,?)/,
+        tmpl: context => `$1,
+$2${context.tenantNameLowerFirst}: [ undefined ]$3`
+    },
+    {
+        type: 'replaceContent',
+        target: /(\n(\s*)this.account.email = this.settingsForm.get\('email'\)!.value;)/,
+        tmpl: context => `$1
+$2this.account.${context.tenantNameLowerFirst} = this.settingsForm.get('${context.tenantNameLowerFirst}')!.value;`
+    },
+    {
+        type: 'replaceContent',
+        target: /(\n(\s*)email: account.email)(,?)/,
+        tmpl: context => `$1,
+$2${context.tenantNameLowerFirst}: account.${context.tenantNameLowerFirst}$3`
+    }
+];
+
+module.exports = {
+    version: '>=6.6.0',
+    file,
+    tmpls
+};
