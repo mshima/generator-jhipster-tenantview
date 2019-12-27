f4c81d09b62795c8134df77094fb9399d38e7d6e
Marcelo Boveto Shima <marceloshima@gmail.com>
2019-12-27 15:30:12 -0200

    v6.6.0 support: add v6.6.0 settings.component.spec.ts.js

---
+++ generators/client/patcher/angular/tenant_add_to_account/protractor/settings.component.spec.ts.js
@@ -0,0 +1,27 @@
+const jhipsterEnv = require('generator-jhipster-customizer');
+
+const jhipsterConstants = jhipsterEnv.constants;
+
+const file = context => `${jhipsterConstants.CLIENT_TEST_SRC_DIR}spec/app/account/settings/settings.component.spec.ts`;
+
+const tmpls = [
+    {
+        type: 'replaceContent',
+        target: /(\n(\s*)imageUrl: '')(,?)/,
+        tmpl: context => `$1,
+$2${context.tenantNameLowerFirst}: 'MockCompany'$3`
+    },
+    {
+        type: 'replaceContent',
+        target: /(\n(\s*)const settingsFormValues = {)/,
+        tmpl: context => `$1
+$2    ${context.tenantNameLowerFirst}: 'MockCompany',`
+    }
+];
+
+module.exports = {
+    condition: context => context.testFrameworks.indexOf('protractor') !== -1,
+    version: '>=6.6.0',
+    file,
+    tmpls
+};
