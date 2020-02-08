b5ed9f3f1eb2794bc0c7658dc694870329d327c9
Marcelo Shima <marceloshima@gmail.com>
2020-02-08 09:55:08 -0300

    Update administration.spec.ts.js

--- generators/client/patcher/angular/tenant_add_test/administration.spec.ts.js
+++ generators/client/patcher/angular/tenant_add_test/administration.spec.ts.js
@@ -12,7 +12,7 @@ const tmpls = [
         target: context => "it('should load metrics', async () => {",
         tmpl: context => `it('should load ${context.tenantNameLowerFirst} management', async () => {
         await navBarPage.clickOnAdmin('${context.tenantFileName}');
-        const expect1 = context.tenantNamePluralUpperFirst;
+        const expect1 = `'${context.tenantNamePluralUpperFirst}'`;
         const value1 = await element.all(by.css('h2#page-heading span')).first().getText();
         expect(value1).to.eq(expect1);
     });\n`
