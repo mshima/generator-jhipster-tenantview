e88fb962cfbb227b2070c7ed04f632cf11ff8378
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-05-23 02:17:58 -0300

    Remove files for old jhipsters.

--- generators/client/patcher/angular/files.js
+++ generators/client/patcher/angular/files.js
@@ -1,19 +1,17 @@
-const jhipsterEnv = require('generator-jhipster-customizer');
-
-const jhipsterConstants = jhipsterEnv.constants;
-
 module.exports = {
-  files: {
-    tenant_admin_menu: [
-      {
-        path: jhipsterConstants.ANGULAR_DIR,
-        templates: [
-          {
-            file: 'tenant-admin/_tenant-admin-routing.module.ts',
-            renameTo: generator => `${generator.tenantNameLowerFirst}-admin/${generator.tenantNameLowerFirst}-admin-routing.module.ts`
-          }
-        ]
-      }
-    ]
+  files: context => {
+    return {
+      tenant_admin_menu: [
+        {
+          path: context.constants.ANGULAR_DIR,
+          templates: [
+            {
+              file: 'tenant-admin/_tenant-admin-routing.module.ts',
+              renameTo: generator => `${generator.tenantNameLowerFirst}-admin/${generator.tenantNameLowerFirst}-admin-routing.module.ts`
+            }
+          ]
+        }
+      ]
+    };
   }
 };
--- generators/server/patcher/files.js
+++ generators/server/patcher/files.js
@@ -1,20 +1,18 @@
-const jhipsterEnv = require('generator-jhipster-customizer');
-
-const jhipsterConstants = jhipsterEnv.constants;
-
 module.exports = {
-  files: {
-    tenant_base: [
-      // Copy over aspect
-      {
-        path: jhipsterConstants.SERVER_MAIN_SRC_DIR,
-        templates: [
-          {
-            file: 'package/aop/_tenant/_UserAspect.java',
-            renameTo: generator => `${generator.packageFolder}/aop/${generator.tenantNameLowerFirst}/UserAspect.java`
-          }
-        ]
-      }
-    ]
+  files: context => {
+    return {
+      tenant_base: [
+        // Copy over aspect
+        {
+          path: context.constants.SERVER_MAIN_SRC_DIR,
+          templates: [
+            {
+              file: 'package/aop/_tenant/_UserAspect.java',
+              renameTo: generator => `${generator.packageFolder}/aop/${generator.tenantNameLowerFirst}/UserAspect.java`
+            }
+          ]
+        }
+      ]
+    };
   }
 };
