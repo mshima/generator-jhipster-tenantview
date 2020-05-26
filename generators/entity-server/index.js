03460915141f5206d78ac04a63ee22da6ac352bf
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-05-25 22:21:59 -0300

    Fixes

--- generators/entity-server/index.js
+++ generators/entity-server/index.js
@@ -3,9 +3,6 @@ const chalk = require('chalk');
 const debug = require('debug')('tenantview:entity:server');
 const path = require('path');
 const customizer = require('generator-jhipster-customizer');
-const JHipsterEntity = require('generator-jhipster-customizer/lib/jhipster-entity');
-
-const setupTenantVariables = require('../multitenancy-utils').setupTenantVariables;
 
 const generator = 'entity-server';
 
@@ -14,10 +11,6 @@ module.exports = {
     const packagePath = env.getPackagePath('jhipster:app');
     // eslint-disable-next-line global-require
     const needleServer = require(`${packagePath}/generators/server/needle-api/needle-server`);
-    // eslint-disable-next-line global-require
-    const constants = require(`${packagePath}/generators/generator-constants`);
-
-    const SERVER_MAIN_SRC_DIR = constants.SERVER_MAIN_SRC_DIR;
 
     const TenantisedNeedle = class extends needleServer {
       addEntityToTenantAspect(generator, tenantAwareEntity) {
@@ -41,34 +34,30 @@ module.exports = {
       constructor(args, options) {
         super(args, options);
 
-        const entityName = this._.upperFirst(args[0]);
-        debug(`Initializing ${generator} ${entityName}`);
+        this.entityName = this._.upperFirst(args[0]);
+        debug(`Initializing ${generator} ${this.entityName}`);
         // Fix {Tenant}Resource.java setting ENTITY_NAME as 'admin{Tenant}'
         this.skipUiGrouping = true;
 
         // Set side-by-side blueprint
         this.sbsBlueprint = true;
 
-        this.entityConfig = this.createStorage(`.jhipster/${entityName}.json`);
-        this.entityConfig.set('name', entityName);
+        this.entityConfig = this.createStorage(`.jhipster/${this.entityName}.json`);
+        this.entityConfig.set('name', this.entityName);
 
-        this.entity = this.jhipsterFs.getEntity(entityName);
+        this.entity = this.jhipsterFs.getEntity(this.entityName);
+
+        const tenantName = this.blueprintConfig.get('tenantName');
+        this.tenant = this.jhipsterFs.getEntity(tenantName);
+        this.isTenant = this.entityName === tenantName;
       }
 
       get writing() {
         return {
-          tenant() {
-            const tenantName = this.blueprintConfig.get('tenantName');
-            this.tenant = this.jhipsterFs.getEntity(tenantName);
-          },
-
-          /* Tenant variables */
-          setupTenantVariables,
-
           // Make the necessary server code changes
           customServerCode() {
             const tenantisedNeedle = new TenantisedNeedle(this);
-            if (this.tenantAware) {
+            if (this.entity.definitions.tenantAware) {
               tenantisedNeedle.addEntityToTenantAspect(this, this.name);
             } else if (this.isTenant) {
               this.addConstraintsChangelogToLiquibase(`${this.entity.changelogDate}-1__user_${this.tenant.entityClass}_constraints`);
